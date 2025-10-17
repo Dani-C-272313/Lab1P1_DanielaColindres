c772c18cecdfb844d0e7d0925afe2f24c3afb3f4
Dani-C-272313 <dani.colindrez27@gmail.com>
2025-10-17 16:32:19 -0600

    Ejercicio 1 terminado

--- Lab1P1_DanielaColindres/src/lab1p1_danielacolindres/Lab1P1_DanielaColindres.java
+++ Lab1P1_DanielaColindres/src/lab1p1_danielacolindres/Lab1P1_DanielaColindres.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package lab1p1_danielacolindres;
-
+import java.util.Scanner ;
 /**
  *
  * @author Hogar
@@ -15,8 +15,98 @@ public class Lab1P1_DanielaColindres {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
         System.out.println("Hello World");
+        
+        int opcion = 0;
+        
+        while(opcion != 2){
+            
+            System.out.println("MENU DE EJERCICIOS");
+            System.out.println("1. Division Modificada");
+            System.out.println("2 SALIR");
+            System.out.println("Seleccione una opcion");
+            opcion = leer.nextInt();
+            
+            // Opcion 1: Division Modificada 
+            if(opcion==1){
+               System.out.println("Ingrese el dividendo: ");
+            int dividendo = leer.nextInt();
+               
+               System.out.println("Ingrese el divisor: ");
+               int divisor = leer.nextInt();
+               
+               int cociente = 0;
+               int residuo = dividendo; 
+               
+               while(residuo>=divisor){
+                   residuo = residuo - divisor; 
+                   // Es lo mismo que usar cociente ++
+                   cociente = cociente +1;
+                   
+               }
+               
+               if (residuo==0) {
+                   System.out.println("El resultado de la division es: "+ cociente);
+               }
+               else { 
+                   System.out.println("El resultado de la dicisiom es: " + cociente + " con un residuo de "+ residuo);
+               }
+               
+               // FIn de la division modificada 
+        
+        }
+        
+        
+        
+        if (opcion==2){
+            System.out.println("Ingrese cantidad de numeros: ");  
+            int n = leer.nextInt(); 
+            int a = 0;
+            int b = 1;
+            int c = 0;
+            int contador = 0;
 
-    }
-    
+            if (contador==0){
+                while(contador<n+2){
+                    if (contador==0){
+                        System.out.println("0");
+
+                    }
+
+                    if (contador ==1){
+                        System.out.println("1");
+
+                    }
+
+                    if (contador>1){
+                        c = a+b+1;
+                        System.out.println(c);
+                        a=b;
+                        b=c;
+                        c=0;
+                    } 
+                    contador=contador+1;
+                
+             
+                }
+                
+            }
+            
+        }
+        if (opcion == 3){
+            System.out.println("Salir de Programa");
+            // cierre del ciclo 
 }
+            
+        }
+  
+               
+       
+              
+        
+
+        
+    }
+}   
+
